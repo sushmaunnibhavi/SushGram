50331a6f16ceecfe5d587fdf9fd0569f3faa7c3e
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2020-12-30 15:47:41 +0530

    Update UploadForm.js

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import ProgressBar from './ProgressBar';
 
 const UploadForm = () => {
+//useState hook used to store file in local piece of state
+    //file is the state and we use setFile to update that state, then using useState set the initial value of file to null
     const [file,setFile] =useState(null);
     const [error,setError]=useState(null);
 
     const types=['image/png','image/jpeg'];
 
     const changeHandler = (e) => {
+        //Only store the first file selected
         let selected=e.target.files[0];
+        //Only update the file value when we have a file selected
         if(selected && types.includes(selected.type)){
             setFile(selected);
             setError('');
