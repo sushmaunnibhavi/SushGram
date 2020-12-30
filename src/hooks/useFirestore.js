7df04c2c9303ac9ea2c66961fac0e8ee2afdda63
Sushma Unnibhavi <sushmaunnibhavi@gmail.com>
2020-12-30 17:12:19 +0530

    Update useFirestore.js

--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -5,17 +5,19 @@ const useFIrestore = (collection) =>{
     const [docs,setDocs]=useState([]);
 
     useEffect(()=>{
+        //Snapshot object represents a snapshot at that moment in time
      const unsub=projectFirestore.collection(collection)
         .orderBy('createdAt','desc')
         .onSnapshot((snap)=>{
             let documents=[];
             snap.forEach(doc=>{
+                //We use ... which is a spread operator which takes all the properties of data and includes it
                 documents.push({...doc.data(),id:doc.id})
             });
             setDocs(documents);
         }) ;
 
-        return ()=>unsub();
+        return ()=>unsub();//Unsubscribe from the collection when we no longer use it. When do we use it? When we unmount imagegrid
     },[collection])
     return {docs};
 }
